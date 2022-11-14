c0ea8487625bd999decb22c3e957baac8dc6e9a4
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-11-14 08:33:48 -0600

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -1179,7 +1179,7 @@ if (login.value != null &&  login.value != '' && fname.value != null &&  fname.v
       document.getElementById('get_msg').style.display = 'block';
           document.getElementById('get_id2').style.display = 'none';
  document.getElementById('loginlabel').innerText = 'Aqua Employee User ID OR Keyword';
-  document.getElementsByName('login')[0].placeholder = '[KEYWORDS] today, name, date, all, inactive,logname';
+  document.getElementsByName('login')[0].placeholder = '[KEYWORDS] today, name, date, all, inactive, logname, logall';
 	       	          document.getElementById('emaillabel').style.display = 'none';
 	          document.getElementById("login").addEventListener("keypress", getSchedule2);
 	       document.getElementById("login").focus();
